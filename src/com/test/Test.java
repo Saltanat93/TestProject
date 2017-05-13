8e6c2f184aaa7a04b6abea042352f010e8d92834
Saltanat93 <kasabolotova93@gmail.com>
2017-05-13 17:20:39 -0400

    summing

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -3,6 +3,24 @@ package com.test;
 public class Test {
 	public static void main(String[] args) {
 		
+		
+		int[]k={2,2,2,2};
+		getNum(k);
+		
+	}
+		
+		public static void getNum(int []num){
+			int sum=0;
+			for(int nums:num){
+				sum+=nums;
+				
+			}
+			System.out.println(sum);
+			
+			
+		
+		
+		
 	}
 
 }
